1ffabb97848e017a72ed677544fba961824870d9
Raphael Berube <raph.berube@gmail.com>
2026-07-21 16:57:19 +0000

    refactor: linting

--- src/group.ts
+++ src/group.ts
@@ -85,14 +85,13 @@ export function groupWordsIntoTurns(
   };
 
   for (const word of words) {
-    const startsNewTurn =
+    if (
       current === null ||
       word.speaker !== current.speaker ||
       (word.startTime !== undefined &&
         current.endTime !== undefined &&
-        word.startTime - current.endTime > maxGapSeconds);
-
-    if (startsNewTurn) {
+        word.startTime - current.endTime > maxGapSeconds)
+    ) {
       flushCurrentTurn();
       current = {
         speaker: word.speaker,
@@ -101,8 +100,8 @@ export function groupWordsIntoTurns(
         endTime: word.endTime,
       };
     } else {
-      current!.words.push(word.word);
-      current!.endTime = word.endTime;
+      current.words.push(word.word);
+      current.endTime = word.endTime;
     }
   }
 
